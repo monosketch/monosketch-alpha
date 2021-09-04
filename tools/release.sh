df506965034359542bdde42c2f1ed30fa8ec5d7b
Tuan Chau <tunachauict@gmail.com>
2021-09-04 14:13:25 +0900

    Update release script to move file into a sub folder

--- tools/release.sh
+++ tools/release.sh
@@ -1,10 +1,21 @@
+# ./release.sh <path-to-src> <commit comment>
 src=$1
 git config user.email "tunachauict@gmail.com"
 git config user.name "Tuan Chau"
 
-mv $src/main.css .
-mv $src/MonoSketch.js .
-mv $src/MonoSketch.js.map .
+hash=$(git rev-parse --short HEAD)
+dest="v1-$hash"
+
+find . -name MonoSketch.js | xargs git rm --cached
+find . -name MonoSketch.js.map | xargs git rm --cached
+find . -name main.css | xargs git rm --cached
+
+mkdir $dest
+mv $src/main.css ./$dest/
+mv $src/MonoSketch.js ./$dest/
+mv $src/MonoSketch.js.map ./$dest/
+
+sed -e  "s/scriptdir/$dest/g" template_index.html > index.html
 
 git add -A
 git commit -m "Update from $2"
